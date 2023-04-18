5348eb44074f751edb7433d286890b7de3d9dd15
Roberto <roberto@chekin.com>
2023-04-17 22:24:28 -0300

    Creating Sort select with react select library

--- src/pages/PlanetsListView/PlanetsListView.tsx
+++ src/pages/PlanetsListView/PlanetsListView.tsx
@@ -5,6 +5,49 @@ import { AVAILABLE_CLIMATES } from "../../utils/constants";
 import PrevNextButtons from "../../components/PrevNextButtons";
 import { ORDER_ORIENTATION_TYPES } from "../../types/common";
 import { Header } from "../../components/Header/Header";
+import Select, { SingleValue } from "react-select";
+
+type SelectOption = SingleValue<{
+  value: SORT_OPTIONS;
+  label: string;
+  data: { order: ORDER_ORIENTATION_TYPES };
+}>;
+//  {
+//   value: SORT_OPTIONS;
+//   label: string;
+//   data: {
+//     order: ORDER_ORIENTATION_TYPES;
+//   };
+// };
+enum SORT_OPTIONS {
+  ascAlphabet = "asc_alphabet",
+  descAlphabet = "desc_alphabet",
+  ascDiameter = "asc_diameter",
+  descDiameter = "desc_diameter",
+}
+
+const sortOptions = [
+  {
+    value: SORT_OPTIONS.ascAlphabet,
+    label: "A-Z",
+    data: { order: ORDER_ORIENTATION_TYPES.asc },
+  },
+  {
+    value: SORT_OPTIONS.descAlphabet,
+    label: "Z-A",
+    data: { order: ORDER_ORIENTATION_TYPES.desc },
+  },
+  {
+    value: SORT_OPTIONS.ascDiameter,
+    label: "Diameter Ascending",
+    data: { order: ORDER_ORIENTATION_TYPES.asc },
+  },
+  {
+    value: SORT_OPTIONS.descDiameter,
+    label: "Diameter Descending",
+    data: { order: ORDER_ORIENTATION_TYPES.desc },
+  },
+];
 
 function PlanetsListView() {
   const {
@@ -18,9 +61,33 @@ function PlanetsListView() {
     resetFilters,
   } = usePlanets();
 
+  const onSort = (event: SelectOption) => {
+    if (!event) {
+      resetFilters();
+      return;
+    }
+    switch (event.value) {
+      case SORT_OPTIONS.ascDiameter:
+      case SORT_OPTIONS.descDiameter:
+        orderByDiameter(event.data.order);
+        break;
+      case SORT_OPTIONS.ascAlphabet:
+      case SORT_OPTIONS.descAlphabet:
+      default:
+        orderByName(event.data.order);
+        break;
+    }
+  };
+
   return (
     <>
-    <Header />
+      <Header />
+      <Select
+        options={sortOptions}
+        isClearable
+        isSearchable
+        onChange={onSort}
+      />
       {Boolean(planets?.length) &&
         planets?.map((planet, index) => {
           return (
